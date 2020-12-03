8bb5cd0f0ad134b06f223ae2c7cab16ca0d08e88
Beresa Abebe <roobaanuuf@gmail.com>
2020-12-03 18:50:28 +0800

    Add Database and Table

---
+++ app/src/main/java/com/coolweather/android/db/City.java
@@ -0,0 +1,40 @@
+package com.coolweather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class City extends DataSupport {
+    private int id, cityCode, provinceCode;
+    private String cityName;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+}
